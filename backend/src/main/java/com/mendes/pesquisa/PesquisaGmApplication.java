3fb418875c03e3adbf575b9cea143664235990aa
francisco-dvh <chicao_nfvsf@hotmail.com>
2020-09-14 22:04:22 -0300

    Project created

---
+++ backend/src/main/java/com/mendes/pesquisa/PesquisaGmApplication.java
@@ -0,0 +1,13 @@
+package com.mendes.pesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaGmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaGmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mendes/pesquisa/PesquisaGmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes.pesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaGmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
